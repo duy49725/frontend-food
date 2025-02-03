bb0647f9af3f8eebff2f4af950ab7dd16a7d6bdb
duy49725 <duy49725@gmail.com>
2025-02-03 22:36:24 +0700

    first commit

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,22 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import Layout from "./layouts/layout";
+import HomePage from "./pages/HomePage";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
+import UserProfilePage from "./pages/UserProfilePage";
+import ProtectRoute from "./auth/ProtectedRoute";
+
+const AppRoutes = () => {
+    return(
+        <Routes>
+            <Route element={<ProtectRoute />}>
+                <Route path="/user-profile" element={<Layout><UserProfilePage /></Layout>}/>
+            </Route>
+            <Route path="/auth-callback" element={<Layout><AuthCallbackPage /></Layout>}/>
+            <Route path="/" element={<Layout><HomePage /></Layout>} />
+            <Route path="/user-profile" element={<span>User Profile Page</span>} />
+            <Route path="/*" element={<Navigate to="/" />} />
+        </Routes>
+    )
+}
+
+export default AppRoutes;
---
+++ src/components/MobileNav.tsx
@@ -0,0 +1,56 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { CircleUserRound, Menu } from "lucide-react"
+import { Separator } from "./ui/separator"
+import { useAuth0 } from "@auth0/auth0-react"
+import MobileNavLink from "./MobileNavLink"
+ 
+export function MobileNav() {
+  const {isAuthenticated, loginWithRedirect, user} = useAuth0();
+  return (
+    <Sheet>
+        <SheetTrigger>
+            <Menu className="text-orange-500 size-10" />
+        </SheetTrigger>
+        <SheetContent className="space-y-3">
+            <SheetTitle>
+                {
+                  isAuthenticated ? (
+                      <span className="flex items-center font-bold gap-2">
+                        <CircleUserRound className="text-orange-500" />
+                      </span>
+                  ) : (
+                    <span>Welcome to MernEats.com!</span>
+                  )
+                }
+            </SheetTitle>
+            <Separator />
+            <SheetDescription className="flex flex-col gap-4">
+              {
+                isAuthenticated ? (
+                  <MobileNavLink />
+                ) : (
+                  <Button
+                    onClick={() => loginWithRedirect()}
+                    className="flex-1 font-bold bg-orange-500"
+                  > 
+                    Log In
+                  </Button>
+                )
+              }
+            </SheetDescription>
+        </SheetContent>
+    </Sheet>
+  )
+}
